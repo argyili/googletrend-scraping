7472205e0bf4b391807a4b72c9109cbfa3a4fe44
argyi <argyi.liaiyi@gmail.com>
2021-07-06 16:06:04 +0900

    Add geographic information.

---
+++ kankyo/excel.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from pandas.io.pytables import IndexCol
+
+
+if __name__ == '__main__':
+    totalDf = pd.DataFrame()
+    for i in range(54):
+        df = pd.read_excel('ame_master.xlsx', sheet_name=i+3, usecols=[1,6,7,8,9], header = None)
+        # df = pd.read_excel('ame_master.xlsx', sheet_name=i+3, usecols=[1,6,7,8,9], header = None, index_col=0)
+        df = df.drop(df.index[[0,1,2]])
+        totalDf = pd.concat([totalDf, df], axis=0)
+
+    totalDf = totalDf.reset_index(drop=True)
+    totalDf.to_csv('geoData.csv', index=False)
+    print(totalDf)
+    
